d9e83fd1fb12b4040fc29a1a6c0292b0d900b132
Mateus Medeiros <50000060+MateusX10@users.noreply.github.com>
2023-02-13 17:51:55 -0300

    Update ex5.cpp

--- exercicios_a_comentar/ex5.cpp
+++ exercicios_a_comentar/ex5.cpp
@@ -1,57 +1,57 @@
 //algoritmo "ex5"
-// FunÁ„o : Escrever um algoritmo que leia o nome de um aluno e as notas das trÍs //provas que ele obteve no semestre. No final
-// informar o nome do aluno e a sua mÈdia (aritmÈtica)
-// Autor : EU
-// Data :        
-// SeÁ„o de DeclaraÁıes 
+// Fun√ß√£o : Escrever um algoritmo que leia o nome de um aluno e as notas das tr√™s //provas que ele obteve no semestre. No final
+// informar o nome do aluno e a sua m√©dia (aritm√©tica)
+// Autor :  Mateus Henrique de Souza Medeiros (apenas comentei o c√≥digo, o c√≥digo em si n√£o fui eu quem desenvolveu)
+// Data : 13/02/23 
+// Se√ß√£o de Declara√ß√µes 
 
 //Abaixo, temos algumas bibliotecas
 
-//Biblioteca para  a manipulÁ„o do teclado e mouse
+//Biblioteca para  a manipul√ß√£o do teclado e mouse
 #include <stdio.h>
 //I/O do console
 #include <conio.h>
-//FunÁıes matem·ticas possivelmente uteis
+//Fun√ß√µes matem√°ticas possivelmente uteis
 #include <math.h>
-//Entrada e saÌda de dados
+//Entrada e sa√≠da de dados
 #include <iostream>
 
-//FunÁ„o principal
+//Fun√ß√£o principal
 main()
       {
-    //Define uma vari·vel do tipo str/char de atÈ 100 caracters
+    //Define uma vari√°vel do tipo str/char de at√© 100 caracters
       char aluno[100];
-      //Define outra vari·vel, mas desta vez do tipo ponto flutuante
+      //Define outra vari√°vel, mas desta vez do tipo ponto flutuante
       float nota,media;
       //Define VAR do tipo inteiro
       int x;
 
       // Inicio COmandos
-      //Imprime uma espÈcie de cabeÁalho
+      //Imprime uma esp√©cie de cabe√ßalho
       printf("============== Pau de aco System ==============\n");
       //Printa e pede ao user o nome do aluno
       printf("Digite o nome do aluno: ");
-      //FunÁ„o para a entrade de dados, haver· nesse caso o input do nome do aluno
+      //Fun√ß√£o para a entrade de dados, haver√° nesse caso o input do nome do aluno
       scanf("%s",&aluno);
-      //Atribui 0 a vari·vel "media"
+      //Atribui 0 a vari√°vel "media"
       media=0;
-      //Faz um for (laÁo) que pede a nota de 3 alunos
+      //Faz um for (la√ßo) que pede a nota de 3 alunos
       for(x=1;x<=3;x++)
       {
       	//Imprime uma mensagem e pede ao user que informe uma nota ao aluno
       printf("Informe Nota:\n");
-      //Entrada de dados por parte de usu·rio para a vari·vel nota
+      //Entrada de dados por parte de usu√°rio para a vari√°vel nota
       scanf("%f",&nota);
-      //Calcula a mÈdia 
+      //Calcula a m√©dia 
       media=((media)+(nota));
       }
-      //Imprime caracters estÈticamente atraentes a muito programadores, a depender do ponto de vista
+      //Imprime caracters est√©ticamente atraentes a muito programadores, a depender do ponto de vista
       printf("============== Media de alunos ==============\n");
-      //Imprine na tela a mÈdia
+      //Imprine na tela a m√©dia
       printf("Media:%f\n",((media)/3));
-      //O "tÌtulo/cabeÁalho" que complementa o cabeÁalho da linha 49
+      //O "t√≠tulo/cabe√ßalho" que complementa o cabe√ßalho da linha 49
       printf("============== Media de alunos ==============\n");
-      //D· uma pausa no sistema atÈ que o user pressione alguma tecla 
+      //D√° uma pausa no sistema at√© que o user pressione alguma tecla 
       system("PAUSE > null");  
       }
 
