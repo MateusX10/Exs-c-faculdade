63f17479103787a74b43bb21943ff891272aeccd
Mateus Medeiros <50000060+MateusX10@users.noreply.github.com>
2023-02-13 17:50:25 -0300

    Update ex3.cpp

--- exercicios_a_comentar/ex3.cpp
+++ exercicios_a_comentar/ex3.cpp
@@ -1,37 +1,37 @@
-// Função : Escrever um algoritmo para determinar o consumo médio de um automóvel //sendo fornecida a distância //total percorrida pelo automóvel e o total de combustível //gasto.
-// Autor :EU
-// Data :        
-// Seção de Declarações 
+// FunÃ§Ã£o : Escrever um algoritmo para determinar o consumo mÃ©dio de um automÃ³vel //sendo fornecida a distÃ¢ncia //total percorrida pelo automÃ³vel e o total de combustÃ­vel //gasto.
+// Autor :  Mateus Henrique de Souza Medeiros (apenas comentei o cÃ³digo, o cÃ³digo em si nÃ£o fui eu quem desenvolveu)
+// Data : 13/02/23 
+// SeÃ§Ã£o de DeclaraÃ§Ãµes 
 
-//Manipulação de entrada e saíde de dados através do mouse e teclado
+//ManipulaÃ§Ã£o de entrada e saÃ­de de dados atravÃ©s do mouse e teclado
 #include <stdio.h>
 //I/O do console
 #include <conio.h>
-//Biblioteca que permite o uso de funções matemáticas
+//Biblioteca que permite o uso de funÃ§Ãµes matemÃ¡ticas
 #include <math.h>
-//Biblioteca de entreada e saída de dados
+//Biblioteca de entreada e saÃ­da de dados
 #include <iostream>
 
-//função principal
+//funÃ§Ã£o principal
 main()
 {	
-	//define 3 variáveis do tipo inteiro
+	//define 3 variÃ¡veis do tipo inteiro
       int distancia,combustivel,cal;
       //Printa uma mensagem na tela
       printf("<><><><><>Calculo de consumo medio de combustivel<><><><><>\n");
-      //Pede ao usuário a distância percorrida
+      //Pede ao usuÃ¡rio a distÃ¢ncia percorrida
       printf("Digite distancia percorrida:");
-      //Função para a entrada de dados (resposta do usuário) sobre a distancia (inteiro)
+      //FunÃ§Ã£o para a entrada de dados (resposta do usuÃ¡rio) sobre a distancia (inteiro)
       scanf("%d",&distancia);
-      //printa e pede ao usuário o quantidade de combustivel gasto
+      //printa e pede ao usuÃ¡rio o quantidade de combustivel gasto
       printf("Digite combustivel gasto:");
-      //Função que permite ao usuário fazer o input de dados de combustível
+      //FunÃ§Ã£o que permite ao usuÃ¡rio fazer o input de dados de combustÃ­vel
       scanf("%d",&combustivel);
-      //calcula o consumo médio
+      //calcula o consumo mÃ©dio
       cal=(distancia/combustivel);
-      //Printa na tela o consumo médio
+      //Printa na tela o consumo mÃ©dio
       printf("Cosumo Medio:%d",cal);
-//Dá um break/pausa pro sistema, para ele "descansar" (apenas brincando)
+//DÃ¡ um break/pausa pro sistema, para ele "descansar" (apenas brincando)
 system("PAUSE > null");     
       }
       
